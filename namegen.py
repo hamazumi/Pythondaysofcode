2098b131dbb57b08c6b0466ff8afd20e7ec1cd26
hamazumi <khamamoto12@gmail.com>
2021-08-23 22:31:22 -0700

    created folder

--- namegen.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the band name generator")
-
-city = input("What city did you grow up in?\n")
-
-pet = input("What is your pets name?\n")
-
-print("You band name is " + city + " " + pet)
