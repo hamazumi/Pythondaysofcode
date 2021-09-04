441be97e3d4d2da57be41aa468c8b8c4a4e15d50
hamazumi <khamamoto12@gmail.com>
2021-09-04 09:47:09 -0700

    partial black jack game

---
+++ beginner/blackJack/main.py
@@ -0,0 +1,28 @@
+from art import logo
+import random
+print(logo)
+
+user = []
+cpu = []
+gameOver = False
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def deal_card():
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+def calculate_score(cards): 
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+for _ in range(2):
+    user.append(deal_card())
+    cpu.append(deal_card())
+
+print(user, cpu)
